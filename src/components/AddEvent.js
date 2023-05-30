51fbcb01345d8a65c0fedc06d59429986b3f8222
Lena Eivy <applena@gmail.com>
2023-05-30 13:35:10 -0700

    dropdown menus for class and subject

--- src/components/AddEvent.js
+++ src/components/AddEvent.js
@@ -6,24 +6,13 @@ import Dropdown from 'react-bootstrap/Dropdown';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-    border: '5px solid black',
-    background: 'light-gray',
-    width: '50%'
-  },
-};
-
 function AddEvent(props) {
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
+  const [eventType, setEventType] = useState('Select Event Type');
+  const [subject, setSubject] = useState('Subject');
+
+  console.log({ subject })
 
   // let subtitle;
   console.log('Add event', props);
@@ -58,59 +47,34 @@ function AddEvent(props) {
 
 
   return (
-    // <Modal
-    //   isOpen={props.displayAddEvent}
-    //   // onAfterOpen={afterOpenModal}
-    //   onRequestClose={() => props.setDisplayAddEvent(false)}
-    //   style={customStyles}
-    //   ariaHideApp={false}
-    //   contentLabel="Example Modal"
-    // >
-    //   <div id="addClass">
-    //     <div className='flex'>
-    //       <h2>Add A Class</h2>
-    //       <div></div>
-    //       <button className='closeButton spaceBetween' onClick={() => props.setDisplayAddEvent(false)}>close</button>`
-    //     </div>
-    //     <form className='flex flexCol' onSubmit={handleSubmit}>
-    //       <label>
-    //         Class Name:
-    //         <input
-    //           // value={name}
-    //           onChange={(e) => updateInput(e, 'name')}
-    //           type="text"
-    //           name="name" />
-    //       </label>
-
-    //       <label>
-    //         Class Description:
-    //         <input
-    //           // value={description}
-    //           onChange={(e) => updateInput(e, 'description')}
-    //           type="text"
-    //           name="description" />
-    //       </label>
-
-    //       <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-    //         <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-    //         <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-    //         <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-    //       </DropdownButton>
-    //       <button className="button">Submit</button>
-    //     </form>
-    //   </div>
-    // </Modal>
     <div
       className="modal show"
       style={{ display: 'block', position: 'initial' }}
     >
       <Modal.Dialog>
         <Modal.Header onHide={() => props.setDisplayAddEvent(false)} closeButton>
-          <Modal.Title>Modal title</Modal.Title>
+          <Modal.Title>Add Event</Modal.Title>
         </Modal.Header>
 
         <Modal.Body>
-          <p>Modal body text goes here.</p>
+          <DropdownButton id="dropdown-basic-button" title={eventType}>
+            <Dropdown.Item
+              onClick={() => setEventType('Class')}
+            >Class</Dropdown.Item>
+            <Dropdown.Item
+              onClick={() => setEventType('Everything Else')}
+            >Everything Else</Dropdown.Item>
+          </DropdownButton>
+
+          {eventType === 'Class' &&
+            <DropdownButton title={subject}>
+              <Dropdown.Item onClick={(e) => setSubject(e.target.textContent)}>ELA</Dropdown.Item>
+              <Dropdown.Item onClick={(e) => setSubject(e.target.textContent)}>Math</Dropdown.Item>
+              <Dropdown.Item onClick={(e) => setSubject(e.target.textContent)}>Science</Dropdown.Item>
+              <Dropdown.Item onClick={(e) => setSubject(e.target.textContent)}>History</Dropdown.Item>
+            </DropdownButton>
+          }
+
         </Modal.Body>
 
         <Modal.Footer>
