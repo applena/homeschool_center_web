93fcde12042a763ed69ff3767e2a6b87ee922200
Lena Eivy <applena@gmail.com>
2023-06-07 13:30:37 -0700

    custom date almost done

--- src/components/AddEvent.js
+++ src/components/AddEvent.js
@@ -8,6 +8,9 @@ import TimePicker from 'react-time-picker';
 import 'react-datetime-picker/dist/DateTimePicker.css';
 import 'react-calendar/dist/Calendar.css';
 import 'react-clock/dist/Clock.css';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import DropdownItem from 'react-bootstrap/esm/DropdownItem';
 
 function AddEvent(props) {
   const [name, setName] = useState('');
@@ -17,9 +20,14 @@ function AddEvent(props) {
   const [time, setTime] = useState();
   const [repeats, setRepeats] = useState(false);
   const [repeatFrequency, setRepeatFrequency] = useState('How Often');
-  const [dayOfWeek, setDayOfWeek] = useState('');
+  const [newSubject, setNewSubject] = useState('');
+  const [repeateNumberFrequency, setRepeatNumberFrequency] = useState(0);
+  const [repeatTimeBlock, setRepeatTimeBlock] = useState('Days');
+  const [repeatsOn, setRepeatsOn] = useState('Monday');
+  const [endsOn, setEndsOn] = useState('never');
+  const [endDate, setEndDate] = useState('');
 
-  console.log({ repeats })
+  console.log({ newSubject })
   console.log('Add event', props);
 
   const handleSubmit = (e) => {
@@ -56,9 +64,91 @@ function AddEvent(props) {
                   <Dropdown.Item onClick={(e) => setSubject(e.target.textContent)}>Math</Dropdown.Item>
                   <Dropdown.Item onClick={(e) => setSubject(e.target.textContent)}>Science</Dropdown.Item>
                   <Dropdown.Item onClick={(e) => setSubject(e.target.textContent)}>History</Dropdown.Item>
+                  <Dropdown.Divider />
+                  <Dropdown.Item onClick={(e) => e.stopPropagation()}><input type="text" name="Create a New Subject" onChange={(e) => { setNewSubject(e.target.value); setSubject(e.target.value) }}></input></Dropdown.Item>
                 </DropdownButton>
               }
             </div>
+
+            {repeatFrequency === 'Custom' &&
+              <Modal.Body>
+                <h2>Custom recurrence</h2>
+                <div className="flex" style={{ alignItems: 'center' }}>Repeat every
+                  <DropdownButton title={repeateNumberFrequency}>
+                    <Dropdown.Item onClick={(e) => setRepeatNumberFrequency(e.target.textContent)}>1</Dropdown.Item>
+                    <Dropdown.Item onClick={(e) => setRepeatNumberFrequency(e.target.textContent)}>2</Dropdown.Item>
+                    <Dropdown.Item onClick={(e) => setRepeatNumberFrequency(e.target.textContent)}>3</Dropdown.Item>
+                    <Dropdown.Item onClick={(e) => setRepeatNumberFrequency(e.target.textContent)}>4</Dropdown.Item>
+                    <Dropdown.Item onClick={(e) => setRepeatNumberFrequency(e.target.textContent)}>5</Dropdown.Item>
+                    <Dropdown.Item onClick={(e) => setRepeatNumberFrequency(e.target.textContent)}>6</Dropdown.Item>
+                    <Dropdown.Item onClick={(e) => setRepeatNumberFrequency(e.target.textContent)}>7</Dropdown.Item>
+                    <Dropdown.Item onClick={(e) => setRepeatNumberFrequency(e.target.textContent)}>8</Dropdown.Item>
+                    <Dropdown.Item onClick={(e) => setRepeatNumberFrequency(e.target.textContent)}>9</Dropdown.Item>
+                    <Dropdown.Item onClick={(e) => setRepeatNumberFrequency(e.target.textContent)}>10</Dropdown.Item>
+                  </DropdownButton>
+
+                  <DropdownButton title={repeatTimeBlock}>
+                    <Dropdown.Item onClick={(e) => setRepeatTimeBlock(e.target.textContent)}>Days</Dropdown.Item>
+                    <Dropdown.Item onClick={(e) => setRepeatTimeBlock(e.target.textContent)}>Weeks</Dropdown.Item>
+                    <Dropdown.Item onClick={(e) => setRepeatTimeBlock(e.target.textContent)}>Months</Dropdown.Item>
+                    <Dropdown.Item onClick={(e) => setRepeatTimeBlock(e.target.textContent)}>Years</Dropdown.Item>
+                  </DropdownButton>
+                </div>
+                <div className="flex" style={{ textAlign: 'left', alignItems: 'center' }}>Repeats on
+                  <DropdownButton title={repeatsOn}>
+                    <Dropdown.Item onClick={(e) => setRepeatsOn(e.target.textContent)}>
+                      Monday
+                    </Dropdown.Item>
+                    <Dropdown.Item onClick={(e) => setRepeatsOn(e.target.textContent)}>
+                      Tuesday
+                    </Dropdown.Item>
+                    <Dropdown.Item onClick={(e) => setRepeatsOn(e.target.textContent)}>
+                      Wednesday
+                    </Dropdown.Item>
+                    <Dropdown.Item onClick={(e) => setRepeatsOn(e.target.textContent)}>
+                      Thursday
+                    </Dropdown.Item>
+                    <Dropdown.Item onClick={(e) => setRepeatsOn(e.target.textContent)}>
+                      Friday
+                    </Dropdown.Item>
+                    <Dropdown.Item onClick={(e) => setRepeatsOn(e.target.textContent)}>
+                      Saturday
+                    </Dropdown.Item>
+                    <Dropdown.Item onClick={(e) => setRepeatsOn(e.target.textContent)}>
+                      Sunday
+                    </Dropdown.Item>
+                  </DropdownButton>
+                </div>
+
+                <div className='flex' style={{ alignItems: 'center' }}>
+                  Ends
+                  <DropdownButton title={endsOn}>
+                    <Dropdown.Item onClick={(e) => setEndsOn(e.target.textContent)}>
+                      never
+                    </Dropdown.Item>
+                    <Dropdown.Item onClick={(e) => setEndsOn(e.target.textContent)}>
+                      On
+                    </Dropdown.Item>
+                    <Dropdown.Item onClick={(e) => setEndsOn(e.target.textContent)}>
+                      After
+                    </Dropdown.Item>
+                  </DropdownButton>
+                </div>
+
+                {endsOn === 'On' &&
+                  <Modal.Body>
+                    <DatePicker selected={endDate} onChange={(date) => setEndDate(date)} />
+                  </Modal.Body>
+                }
+
+                {endsOn === 'After' &&
+                  <Modal.Body>
+                    occurances here
+                  </Modal.Body>
+                }
+
+              </Modal.Body>
+            }
 
             <div className='flex'>
               <input style={{ display: 'inline-block', width: 'auto', marginRight: '10px' }} onClick={() => setRepeats(!repeats)} type="checkbox" />
@@ -69,11 +159,12 @@ function AddEvent(props) {
 
             {repeats &&
               <DropdownButton title={repeatFrequency}>
-                <Dropdown.Item onClick={(e) => setSubject(e.target.textContent)}>Daily</Dropdown.Item>
-                <Dropdown.Item onClick={(e) => setSubject(e.target.textContent)}>Weekly on {props.daySelected}</Dropdown.Item>
-                <Dropdown.Item onClick={(e) => setSubject(e.target.textContent)}>Monthly on {props.ordinal}</Dropdown.Item>
-                <Dropdown.Item onClick={(e) => setSubject(e.target.textContent)}>Annually on {props.month} {props.day}</Dropdown.Item>
-                <Dropdown.Item onClick={(e) => setSubject(e.target.textContent)}>Every weekday (Monday to Friday)</Dropdown.Item>
+                <Dropdown.Item onClick={(e) => setRepeatFrequency(e.target.textContent)}>Daily</Dropdown.Item>
+                <Dropdown.Item onClick={(e) => setRepeatFrequency(e.target.textContent)}>Weekly on {props.daySelected}</Dropdown.Item>
+                <Dropdown.Item onClick={(e) => setRepeatFrequency(e.target.textContent)}>Monthly on {props.ordinal}</Dropdown.Item>
+                <Dropdown.Item onClick={(e) => setRepeatFrequency(e.target.textContent)}>Annually on {props.month} {props.day}</Dropdown.Item>
+                <Dropdown.Item onClick={(e) => setRepeatFrequency(e.target.textContent)}>Every weekday (Monday to Friday)</Dropdown.Item>
+                <Dropdown.Item onClick={(e) => setRepeatFrequency(e.target.textContent)}>Custom</Dropdown.Item>
               </DropdownButton>
             }
 
